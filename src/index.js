ff76f3465b8ce5cae774b32c40b4f273b8fb5c87
seoha.kim <keynotforlock@gmail.com>
2018-11-19 16:32:09 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import greeting from  "./lib.js"
+
+export default function test() {
+  console.log(greeting("Ahoy!"))
+}
---
+++ src/lib.js
@@ -0,0 +1,5 @@
+function greeting(str:string) : string {
+  return str.concat(" Pirates!")
+}
+
+export default greeting;
